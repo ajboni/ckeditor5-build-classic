f44876f042026ef3176f329694d01c308e9dfb96
aboni <mail@aboni.dev>
2020-01-10 00:23:57 -0300

    added desired plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,13 +7,11 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -38,7 +36,6 @@ import Title from '@ckeditor/ckeditor5-heading/src/title';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
 export default class ClassicEditor extends ClassicEditorBase { }
-
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
@@ -99,7 +96,6 @@ ClassicEditor.defaultConfig = {
 			'insertTable',
 			'horizontalLine',
 			'mediaEmbed'
-
 
 		]
 	},
